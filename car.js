eea384dda23fdff1a72f4f32b3a375544f7cbbf2
JDusub83 <jamesbraza@gmail.com>
2017-02-11 01:51:41 -0500

    More changes, bug fixes

--- car.js
+++ car.js
@@ -1,40 +1,40 @@
 class Car {
-	var msInDay = 24*60*60*1000; //ms
-	var oldTime; //a Date.UTC
-	var xPos = 0, xVel = 0, xAccel = 0, yPos = 0, yVel = 0, yAccel = 0;
 	
-  constructor(x,y,radius) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-  }
-
-  function drawCar(){
-
-    var circle = new Graphics();
-    circle.beginFill(0xe74c3c); // Red
-    circle.drawCircle(this.x,this.y,this.radius); // drawCircle(x, y, radius)
-    circle.endFill();
-  }
-
-  function update(){
-	var newTime = new Date.UTC(); // current time in universal time (no timezone)
-	var deltaTime = newTime - oldTime;
-	oldTime = newTime; // updating old time to be current time
+	constructor(x,y,radius) {
+		this.xPos = x;
+		this.yPos = y;
+		this.radius = radius;
+		this.oldTime = new Date.UTC();
+		this.xPos = 0, this.xVel = 0, this.xAccel = 0, this.yPos = 0, this.yVel = 0, this.yAccel = 0;
+	}
+  
 	
-	//Motion Equations
-	xPos = xPos + xVel * deltaTime + 0.5 * xAccel * Math.pow(xAccel,2);
-	yPos = yPos + yVel * deltaTime + 0.5 * yAccel * Math.pow(yAccel,2);
-	xVel = xVel + xAccel * deltaTime;
-	yVel = yVel + yAccel * deltaTime;
-  }
+	
+	drawCar() {
+		var circle = new Graphics();
+		circle.beginFill(0xe74c3c); // Red
+		circle.drawCircle(this.x,this.y,this.radius); // drawCircle(x, y, radius)
+		circle.endFill();
+	}
 
-  getVelocity() {
-    return [xVel, yVel];
-  }
+	update(){
+		var newTime = new Date.UTC(); // current time in universal time (no timezone)
+		var deltaTime = newTime - oldTime;
+		oldTime = newTime; // updating old time to be current time
+		
+		// Motion Equations
+		xPos = xPos + xVel * deltaTime + 0.5 * xAccel * Math.pow(xAccel,2);
+		yPos = yPos + yVel * deltaTime + 0.5 * yAccel * Math.pow(yAccel,2);
+		xVel = xVel + xAccel * deltaTime;
+		yVel = yVel + yAccel * deltaTime;
+	  }
 
-  accelerate(xAcc, yAcc) {
-	this.xAccel = xAcc;
-	this.yAccel = yAcc;
-  }
+	  getVelocity() {
+		return [xVel, yVel];
+	  }
+
+	  accelerate(xAcc, yAcc) {
+		this.xAccel = xAcc;
+		this.yAccel = yAcc;
+	  }
 }
--- drive.js
+++ drive.js
@@ -1,28 +1,26 @@
 class Drive
 {
-  constructor(app)
-  {
-    // Save off pixie application
-    this.app = app;
+  constructor(app){
+		this.app = app; // Save off pixie application
   }
 
-  // This should take a callback to call when the simulation
+  // This should take a callback to call when the simulation - when it what's Levi!
   startSimulation(userCallback) // Setup timed callback to call the user callback and the render function
   {
-	  var delay = 1000/60; // 1000 ms /60 cycles = delay for 60 Hz functionality
-	  var timer = setTimeout(func, delay);
+		var delay = 1000/60; // 1000 ms / 60 cycles = delay for 60 Hz functionality
+		var timer = setTimeout(func, delay);
 	  
 		var carRadius = 4;
 		var vehicle = new Car(0,0,carRadius);
-		Car.drawCar();
+		vehicle.drawCar();
 	  
-	  function func() // kind of like an infinite while loop, where the loop is done @ 60 Hz
-	  {
-		userCallback();
-		
-		Car.update();
-		timer = setTimeout(func, delay); //kind of recursion
-	  }
+		function func() // kind of like an infinite while loop, where the loop is done @ 60 Hz
+		{
+			userCallback();
+
+			vehicle.update();
+			timer = setTimeout(func, delay); //kind of recursion
+		}
 
   }
 }
