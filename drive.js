4804a7f1376f98a1084919c12509277996060689
Levi Burner <leviburner@gmail.com>
2017-02-10 23:18:46 -0500

    Evaluate text based callback upon simulation start

--- drive.js
+++ drive.js
@@ -5,28 +5,10 @@ class Drive {
   }
 
   // This should take a callback to call when the simulation
-  startSimulation() {
-    // load the texture we need
-    PIXI.loader.add('bunny', 'bunny.jpg').load(function(loader, resources) {
-        // This creates a texture from a 'bunny.png' image.
-        var bunny = new PIXI.Sprite(resources.bunny.texture);
-
-        // Setup the position of the bunny
-        bunny.x = app.renderer.width / 2;
-        bunny.y = app.renderer.height / 2;
-
-        // Rotate around the center
-        bunny.anchor.x = 0.5;
-        bunny.anchor.y = 0.5;
-
-        // Add the bunny to the scene we are building.
-        app.stage.addChild(bunny);
-
-        // Listen for frame updates
-        app.ticker.add(function() {
-             // each frame we spin the bunny around a bit
-            bunny.rotation += 0.01;
-        });
-    });
+  startSimulation(userCallback) {
+    // Setup timed callback to call the user call back and the render function
+    userCallback();
   }
+
+
 }
