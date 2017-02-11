0459ea456721318c2d0fb8a291179275b23ef132
Levi Burner <leviburner@gmail.com>
2017-02-11 15:32:34 -0500

    Fix bounds smoother animation

--- drive.js
+++ drive.js
@@ -1,16 +1,16 @@
 class Drive
 {
-  constructor(graphics, scalingFactor,container){
-    this.objects = [new Obstacle(3,3,2,2)];
-    this.endpoint = new Endpoint(1, 1, 2, 2);
+  constructor(app, graphics, scalingFactor,container){
+    this.objects = [new Obstacle(3,3,2,2), new Obstacle(7,7,0.5,0.5)];
+    this.endpoint = new Endpoint(3, 0, 2, 2);
     this.scalingFactor = scalingFactor;
-    this.updateDelay = 1000.0/60.0; // 1000 ms / 60 cycles = delay for 60 Hz functionality
     this.userCallback = null;
     this.lastTimeStamp = 0;
     this.time = null;
     this.graphics = graphics;
     this.container = container;
-    this.cars = [new Car(5, 5, 2, 1.2,this.container)];
+    this.cars = [new Car(0, 0, 2, 1.2,this.container)];
+    this.app = app;
   }
 
   setScalingFactor(scalingFactor)
@@ -23,7 +23,9 @@ class Drive
   {
     this.userCallback = userCallback;
 		this.lastTimeStamp = Date.now();
-    this.timer = setTimeout(this.update.bind(this), this.updateDelay);
+
+    // Listen for animate update
+    this.app.ticker.add(this.update, this);
   }
 
   update()
@@ -31,18 +33,28 @@ class Drive
       var currentTime = Date.now();
       var deltaTime = (currentTime - this.lastTimeStamp)/1000.0;
       this.lastTimeStamp = currentTime;
-      console.log("FPS: " + 1.0/deltaTime);
 
       this.graphics.clear();
 
       for(var i = 0; i < this.objects.length; i++)
       {
         var object = this.objects[i];
-        object.update(deltaTime);
         object.draw(this.graphics, this.scalingFactor);
       }
 
       this.endpoint.draw(this.graphics, this.scalingFactor);
+
+      for(var i = 0; i < this.cars.length; i++)
+      {
+        var car = this.cars[i];
+        car.draw(this.scalingFactor);
+      }
+
+      for(var i = 0; i < this.objects.length; i++)
+      {
+        var object = this.objects[i];
+        object.update(deltaTime);
+      }
 
       // 0 is regular, 1 is over, 2 is won
       var gameState = 0;
@@ -52,7 +64,6 @@ class Drive
         var car = this.cars[i];
         this.userCallback(car);
         car.update(deltaTime);
-        car.draw(this.scalingFactor);
 
         for(var i = 0; i < this.objects.length; i++)
         {
@@ -72,15 +83,17 @@ class Drive
 
       if(gameState == 0)
       {
-        this.timer = setTimeout(this.update.bind(this), this.updateDelay);
+        //this.timer = setTimeout(this.update.bind(this), this.updateDelay);
       }
       else if(gameState == 1)
       {
         console.log("YOU CRASHED");
+        this.app.ticker.remove(this.update, this);
       }
       else if(gameState == 2)
       {
         console.log("YOU WON");
+        this.app.ticker.remove(this.update, this);
       }
       else
       {
--- endpoint.js
+++ endpoint.js
@@ -3,19 +3,20 @@ class Endpoint
   constructor(x,y,width,height) {
     this.x = x;
     this.y = y;
-    this.length = height;
+    this.height = height;
     this.width = width;
     this.boundingRect = new Rectangle(x, y, width, height);
   }
-  
+
   draw(graphics, scalingFactor) {
-      graphics.beginFill(0x009FE9, 1); // Random color
-      graphics.drawRect(this.x * scalingFactor,this.y * scalingFactor,
-                        this.width * scalingFactor, this.height * scalingFactor);
-      graphics.endFill();
+    graphics.beginFill(0xFF0000, 1); // corresponds to light blue
+    graphics.drawRect(this.x * scalingFactor, this.y * scalingFactor,
+                      this.width * scalingFactor, this.height * scalingFactor);
+    graphics.endFill();
   }
 
   update(deltaTime){
 
   }
 }
+
